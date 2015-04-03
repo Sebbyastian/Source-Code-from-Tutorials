bdaebd281a8c84bb735fccd20ce11dc60862a486
Sebastian Ramadan <plebbyastian@gmail.com>
2015-04-04 10:25:19 +1030

    Update 63_cppBeginners.cpp
    Zero is not an exception.

--- C++/63_cppBeginners.cpp
+++ C++/63_cppBeginners.cpp
@@ -8,14 +8,21 @@ int main()
 		int num1;
 		cout << "Enter first number :" << endl;
 		cin>> num1;
+		if (!cin.good()) {
+			cout << "That's not a number, duh!" << endl;
+			return 0;
+		}
 		
 		int num2;
 		cout << "Enter second number :" << endl;
 		cin>> num2;
+		if (!cin.good()) {
+			cout << "You know what a number is, right?" << endl;
+			return 0;
+		}
 		
 		if(num2 == 0){
-			throw 0;
-		
+			throw std::overflow_error("Division by zero");
 		}
 		
 		cout << num1/num2 << endl;
